b371903543351ba677104449c8945c6f59c37510
Soleiu <himamegu847@gmail.com>
2020-08-04 19:20:50 +0900

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_061509) do
+ActiveRecord::Schema.define(version: 2020_08_03_084034) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
