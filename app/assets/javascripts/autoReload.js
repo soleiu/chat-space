a010333ac9c9016fd3689fe977241e8f49665f7d
Soleiu <himamegu847@gmail.com>
2020-08-20 17:41:28 +0900

    自動更新機能の実装

---
+++ app/assets/javascripts/autoReload.js
@@ -0,0 +1,65 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html = 
+      `<div class="Main-chat__messagebox__info" data-message-id=${message.id}>
+          <div class="Main-chat__messagebox__info__username">
+            ${message.user_name}
+          </div>
+          <div class="Main-chat__messagebox__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Main-chat__messagebox__info">
+          <p class="Main-chat__messagebox__info__content">
+            ${message.content}
+          </p>
+          <img class="Message__image" src="${message.image}">
+        </div>
+      </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="Main-chat__messagebox__info"data-message-id=${message.id}>
+          <div class="Main-chat__messagebox__info__username">
+            ${message.user_name}
+          </div>
+          <div class="Main-chat__messagebox__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Main-chat__messagebox__info">
+          <p class="Main-chat__messagebox__info__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  let reloadMessages = function() {
+    let last_message_id = $('.Main-chat__messagebox__info:last').data("message-id") || 0;
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      
+      if (messages !== 0) {
+        let insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.Main-chat__messagebox').append(insertHTML);
+        $('.Main-chat__messagebox').animate({ scrollTop: $('.Main-chat__messagebox')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
+});
